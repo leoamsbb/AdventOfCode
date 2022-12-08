7f0821da186f4aefbb5ff071daea1e4725604477
leoamsbb <mrunalbadhe24@gmail.com>
2022-12-08 12:13:46 +0530

    refactor: organise imports

--- aoc-2022/src/test/scala/day07/Day07_Spec.scala
+++ aoc-2022/src/test/scala/day07/Day07_Spec.scala
@@ -1,6 +1,5 @@
 package day07
 
-import org.scalatest.Ignore
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 import util.InputReader.readInput
@@ -8,10 +7,6 @@ import util.InputReader.readInput
 class Day07_Spec extends AnyFlatSpec {
   private val day = "day07"
   private val lines = readInput(s"$day/data/input")
-
-  day should "split the input into two correct data" in {
-
-  }
 
   "Puzzle One" should "return correct result" in {
     One.run(lines) shouldBe 95437
