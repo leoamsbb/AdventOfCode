d7e361abe09ad427ad69e499f630f50369199b83
leoamsbb <mrunalbadhe24@gmail.com>
2023-12-03 21:53:04 +0530

    Dummy commit

--- aoc-2023/src/main/scala/day18/Day18.scala
+++ aoc-2023/src/main/scala/day18/Day18.scala
@@ -7,9 +7,9 @@ object Day18 extends App {
   private val day = "day18"
   private val input = readInput(s"$day/data/input")
 
-  val opForFirstPuzzle = One.run(input)
-  println(s"Output for first puzzle: $opForFirstPuzzle")
-
-  val opForSecondPuzzle = Two.run(input)
-  println(s"Output for Second puzzle: $opForSecondPuzzle")
+//  val opForFirstPuzzle = One.run(input)
+//  println(s"Output for first puzzle: $opForFirstPuzzle")
+//
+//  val opForSecondPuzzle = Two.run(input)
+//  println(s"Output for Second puzzle: $opForSecondPuzzle")
 }
--- aoc-2023/src/main/scala/day18/One.scala
+++ aoc-2023/src/main/scala/day18/One.scala
@@ -5,6 +5,6 @@ object One {
   def run(input: List[LavaDroplet]): Int = {
     var map = input.map((_ -> 6)).toMap
 
-
+    0
   }
 }
